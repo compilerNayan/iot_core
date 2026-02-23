94c51d8861a500e39103206664927facebded2b6
Nayan Kurude <compilerNayan@gmail.com>
2026-02-23 12:00:00 +0530

    lkdsalkd

--- include/iot_core/IIoTCoreApp.h
+++ include/iot_core/IIoTCoreApp.h
@@ -1,6 +1,6 @@
 #ifndef IOT_CORE_IIOT_CORE_APP_PUBLIC_H
 #define IOT_CORE_IIOT_CORE_APP_PUBLIC_H
 
-#include "../../internal/01-interface/01-IIoTCoreApp.h"
+#include "../../internal/00-public/01-IoTCoreApp.h"
 
 #endif // IOT_CORE_IIOT_CORE_APP_PUBLIC_H
---
+++ internal/00-public/01-IoTCoreApp.h
@@ -0,0 +1,6 @@
+#ifndef IOT_CORE_IOT_CORE_APP_PUBLIC_H
+#define IOT_CORE_IOT_CORE_APP_PUBLIC_H
+
+#include "../01-interface/01-IIoTCoreApp.h"
+
+#endif // IOT_CORE_IOT_CORE_APP_PUBLIC_H

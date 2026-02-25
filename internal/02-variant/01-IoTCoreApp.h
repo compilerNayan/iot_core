5b111a3a8d3e0a17848bb07baca23dddbb30270d
Nayan Kurude <compilerNayan@gmail.com>
2026-02-25 10:47:14 +0530

    kjdajk

--- internal/02-variant/01-IoTCoreApp.h
+++ internal/02-variant/01-IoTCoreApp.h
@@ -42,7 +42,7 @@ class IoTCoreApp final : public IIoTCoreApp {
     Public IoTCoreApp() {
         AddStartupThread<WiFiHealthCheckerThread>(ThreadPoolCore::System, false);
         //AddStartupThread<LocalServerThread>(ThreadPoolCore::System, false);
-        AddStartupThread<InternetHealthCheckerThread>(ThreadPoolCore::Application, false);
+        AddStartupThread<InternetHealthCheckerThread>(ThreadPoolCore::System, false);
         //AddStartupThread<DeviceTimeSyncThread>(ThreadPoolCore::System, false);
         AddStartupThread<ResponseHandlerThread>(ThreadPoolCore::Application, true);
         //AddStartupThread<CloudServerThread>(ThreadPoolCore::System, true);
